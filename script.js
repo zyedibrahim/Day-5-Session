acab814fc3344d115b3c1b7e0cb361385e19e98d
zyedibrahim <zyedibrahim77@gmail.com>
2022-11-29 19:48:07 +0530

    init

---
+++ script.js
@@ -0,0 +1,244 @@
+// 1 Questions
+
+ //  using anonymous functions
+ // a) print odd numbers in arrays 
+
+//  var arr =[1,2,3,4,5,6,7,8]
+// var result =[];
+// var anny = function(){
+//   for(var i=0;i<arr.length;i++){
+//     if (arr[i]%2!=0){
+  //       result.push(arr[i])
+
+  //     }
+//   }
+//   return result
+// }
+
+// console.log(anny(arr))
+
+
+
+
+ //  using IIFE functions
+ // a) print odd numbers in arrays
+
+
+//  (function() {
+
+
+//   var arr =[1,2,3,4,5,6,7,8]
+// var result =[];
+// var anny = function(){
+//   for(var i=0;i<arr.length;i++){
+//     if (arr[i]%2!=0){
+//       result.push(arr[i])
+//       }
+//   }
+//   return result
+// }
+
+// console.log( "Odd Number is : " +anny(arr))
+
+//     })();
+
+
+
+//====================================
+
+      
+ //  using anonymous functions
+//c) sum all numbers in arrays 
+// var arr =[2,5,6,7,8]
+// var sum=0;
+//  var funct =function(){
+  // for (let i =0; i < arr.length;i++){
+    //     sum += arr[i]; 
+    // }
+    //      return sum
+    //  }
+    //  console.log("sum numbers:"+funct(sum))
+    // -----------------------------------------------------
+    
+    //  using IIFE functions
+    //c) sum all numbers in arrays 
+    // (function() {
+
+    //   var arr =[2,5,6,7,8]
+    //   var sum=0;
+    //     var funct =function(){
+    //   for (let i =0; i < arr.length;i++){
+    //       sum += arr[i]; 
+    //   }
+    //         return sum
+    //     }
+      
+      
+      
+    //     console.log("sum numbers:"+funct(sum))
+      
+    //       })();
+     //========================================================
+     
+     
+    //  using anonymous functions
+    //   g) Remove duplicate from a array 
+    
+    // var arr = ["apple", "mango", "apple",
+    // "orange", "mango", "mango"];
+    
+    // var removeDuplicates= (arr) => {
+      // return arr.filter((item,
+      // index) => arr.indexOf(item) === index);
+      // }
+      
+      // console.log(removeDuplicates(arr));
+      
+      
+      
+     //  using IIFE functions
+     //  // g) Remove duplicate from a array 
+
+    //  (function() {
+
+    //   var arr = ["apple", "mango", "apple",
+    //   "orange", "mango", "mango"];
+      
+    //   var removeDuplicates= (arr) => {
+    //   return arr.filter((item,
+    //   index) => arr.indexOf(item) === index);
+    //   }
+      
+    //   console.log(removeDuplicates(arr));
+      
+      
+    //       })();
+
+//===================================================
+
+
+// using annoymous functions
+//  b) Convert all the strings to title caps in a string array
+
+// var titleCase =function(string){
+  //   var sentence  =string.toLowerCase().split(" ");
+  //   for(var i =0; i<sentence.length; i++){
+    //     sentence[i] = sentence[i][0].toUpperCase() + sentence[i].slice(1);
+    //   }
+    //   console.log(sentence.join(" "));
+    //   return sentence;
+    // }     
+    
+    // titleCase("guvi geek pvt ltd");   
+    
+    
+    
+    // using annoymous functions
+    //  b) Convert all the strings to title caps in a string array
+    
+// (function() {
+
+//   var titleCase =function(string){
+//     var sentence  =string.toLowerCase().split(" ");
+//     for(var i =0; i<sentence.length; i++){
+//       sentence[i] = sentence[i][0].toUpperCase() + sentence[i].slice(1);
+//     }
+//     console.log(sentence.join(" "));
+//     return sentence;
+//   }     
+  
+//   titleCase("guvi geek pvt ltd");   
+  
+  
+//       })();
+  
+
+
+
+
+
+
+//===============================================
+
+
+// using annoymous function 
+// h) Rotate an array by k times
+
+
+// var annoy = function(nums){
+  
+//   var k = nums.length-1;
+//   for(var i =0; i<k;i++){
+//     nums.unshift(nums.pop())
+//   }
+//   return nums
+// }
+
+// console.log(annoy([1,2,3,4,5]))
+
+
+
+// using IIFE function 
+// h) Rotate an array by k times
+
+
+// (function() {
+
+//   var arr = ["apple", "mango", "apple",
+//   "orange", "mango", "mango"];
+  
+//   var removeDuplicates= (arr) => {
+//   return arr.filter((item,
+//   index) => arr.indexOf(item) === index);
+//   }
+  
+//   console.log(removeDuplicates(arr));
+  
+  
+//       })();
+
+
+
+//==================================
+// using anonymous function
+// h) Rotate an array by k times
+
+
+// var annoy = function(nums){
+  
+  //  var k = nums.length-1;
+  //   for(var i =0; i<k;i++){
+    //     nums.unshift(nums.pop())
+    //   }
+    //   return nums
+    // }
+    //  console.log(annoy([1,2,3,4,5]))
+    
+    
+    // using anonymous function
+    // h) Rotate an array by k times
+
+    // (function() {
+
+    //   var annoy = function(nums){
+      
+    //     var k = nums.length-1;
+    //     for(var i =0; i<k;i++){
+    //       nums.unshift(nums.pop())
+    //     }
+    //     return nums
+    //   }
+    //     console.log(annoy([1,2,3,4,5]))
+      
+    //       })();
+      
+
+
+
+
+
+
+
+
+
+
